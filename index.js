4bd353ac5f8ed231f54c7986c6ad0826d135c5d3
Benjamin Mann <8enmann@gmail.com>
2018-10-16 15:41:22 -0700

    json output, mozilla parsing only, extract extra metadata using unfluff

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
-var read = require('node-readability')
-const Readability = require("readability");
+const Readability = require('readability');
+const extractor = require('unfluff');
 var request = require('request-promise-native');
-const jsdom = require("jsdom");
+const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-const UA = "Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36"
-var options = {
-  headers: {
-    'User-Agent': UA
-  }
-}
-
 // Try eg http://localhost:3000/?url=http://www.google.com
 app.get('/', async (req, res) => {
   try {
-    var result;
-    if (req.query.moz) {
-      result = await mozParse(req.query.url)
+    var result = await parse(req.query.url)
+    if (req.query.json) {
+      res.json(result)
     } else {
-      result = await parse(req.query.url)
+      res.send(result.content)
     }
-    res.send(result)
   } catch (e) {
     res.send('failed to parse ' + e)
   }
@@ -31,9 +23,8 @@ app.get('/', async (req, res) => {
 
 // Uncomment for fast testing
 //parse('')
-//mozParse()
 
-async function mozParse(url) {
+async function parse(url) {
   if (!url || url === '') {
     url = 'http://howtonode.org/really-simple-file-uploads'
   }
@@ -59,46 +50,14 @@ async function mozParse(url) {
     })
   //console.log(dom.window.document.querySelector("p").textContent);
   var article = new Readability(dom.window.document).parse();
+  var data = extractor.lazy(response);
+  article.image = data.image()
+  article.author = data.author()
+  article.date = data.date()
   //console.log(article.content)
-  return article.content
+  return article
 
 }
 
-async function parse(url) {
-  if (!url || url === '') {
-    url = 'http://howtonode.org/really-simple-file-uploads'
-  }
-  console.log('fetching ' + url)
-  return new Promise(function(resolve, reject) {
-    read(url, function(err, article, meta) {
-      if (err) {
-        console.log(err)
-        reject(err)
-        return
-      }
-      // Main Article
-      //console.log(article.content);
-      // Title
-      //console.log(article.title);
-
-      // HTML Source Code
-      //console.log(article.html);
-
-      // DOM
-      //console.log(article.document);
-
-      // Response Object from Request Lib
-      //console.log(meta);
-
-      // Close article to clean up jsdom and prevent leaks
-      if (article) {
-        resolve(article.content)
-        article.close()
-        return
-      }
-      reject('empty article')
-    })
-  })
-}
 
 app.listen(port, () => console.log(`http://localhost:${port}`))
